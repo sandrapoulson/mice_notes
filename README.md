3e122fe3235e53a19493a43db40a061885554014
Jack Poulson <poulson@stanford.edu>
2016-03-14 20:43:47 -0700

    Adding the ability to pause/unpause, improving output, adding a simpler means of launching the script and the ability to return the action dictionary, and adding a README

---
+++ README.md
@@ -0,0 +1,53 @@
+### mice_notes
+A simple utility for recording a small selection of states of a mouse in a cage.
+
+#### Running the program
+
+The simplest way to run the program is via the command:
+
+```
+python mice_notes
+```
+
+which immediately starts the recording process. After recording has begun,
+the following keys 
+
+* 'a': Allogrooming (grooming the unconscious mouse)
+
+* 'b': Burrowing (rummaging under bedding or other mouse)
+
+* 'c': Climbing cage
+
+* 'g': Grooming self
+
+* 'n': Nesting behavior (bringing materials or building nest)
+
+* 'r': Rearing (forepaws in air)
+
+* 's': Sitting
+
+* 'o': Other (blank state)
+
+* 'q': Quit the recording process
+
+* ' ': Pause/unpause the recording process
+
+After quitting, if PyPlot is installed, a pie chart of the time spent in each
+action is displayed. Furthermore, more advanced users may want to instead load
+the `mice_notes` module within python (`import mice_notes`) so that the 
+raw dictionary of action time segments can be returned. For example,
+
+```
+import mice_notes
+first_video = mice_notes.start()
+second_video = mice_notes.start()
+```
+
+would allow the data from a first video to be recorded in a dictionary named
+`first_video`, and likewise for the second video. These dictionaries can then
+be probed for the raw data of each action, such as
+
+```
+>>> first_video['o']
+[(0, 0.29196596145629883), (1.7228410243988037, 3.185948133468628)]
+```
--- mice_notes.py
+++ mice_notes.py
@@ -1,122 +1,159 @@
 #!/usr/bin/env python
+import termios, fcntl, sys, os
 
-# NOTE: This was copied from a Stack Overflow post
-def read_key():
-    """Waits for a single keypress on stdin.
-    
-    This is a silly function to call if you need to do it a lot because it has
-    to store stdin's current setup, setup stdin for reading single keystrokes
-    then read the single keystroke then revert stdin back after reading the
-    keystroke.
-    
-    Returns the character of the key that was pressed (zero on
-    KeyboardInterrupt which can happen when a signal gets handled)
-    
-    """
-    import termios, fcntl, sys, os
-    fd = sys.stdin.fileno()
-    flags_save = fcntl.fcntl(fd, fcntl.F_GETFL)
-    attrs_save = termios.tcgetattr(fd)
-    attrs = list(attrs_save)
-    attrs[0] &= ~(termios.IGNBRK | termios.BRKINT | termios.PARMRK | termios.ISTRIP | termios.INLCR | termios.IGNCR | termios.ICRNL | termios.IXON)
-    attrs[1] &= ~termios.OPOST
-    attrs[2] &= ~(termios.CSIZE | termios.PARENB)
-    attrs[2] |= termios.CS8
-    attrs[3] &= ~(termios.ECHONL | termios.ECHO | termios.ICANON | termios.ISIG | termios.IEXTEN)
-    termios.tcsetattr(fd, termios.TCSANOW, attrs)
-    fcntl.fcntl(fd, fcntl.F_SETFL, flags_save & ~os.O_NONBLOCK)
-    try:
-        ret = sys.stdin.read(1)
-    except KeyboardInterrupt:
-        ret = 0
-    finally:
-        termios.tcsetattr(fd, termios.TCSAFLUSH, attrs_save)
-        fcntl.fcntl(fd, fcntl.F_SETFL, flags_save)
+# The routines ready_stdin, read_key, and restore_stdin are a reformulation
+# of the following Stack Overflow answer:
+#     http://stackoverflow.com/a/6599441 
 
-    return ret
+def ready_stdin():
+  fd = sys.stdin.fileno()
+  flags_save = fcntl.fcntl(fd, fcntl.F_GETFL)
+  attrs_save = termios.tcgetattr(fd)
+  attrs = list(attrs_save)
+  attrs[0] &= ~(termios.IGNBRK | \
+                termios.BRKINT | \
+                termios.PARMRK | \
+                termios.ISTRIP | \
+                termios.INLCR  | \
+                termios.IGNCR  | \
+                termios.ICRNL  | \
+                termios.IXON)
+  attrs[1] &= ~termios.OPOST
+  attrs[2] &= ~(termios.CSIZE | \
+                termios.PARENB)
+  attrs[2] |= termios.CS8
+  attrs[3] &= ~(termios.ECHONL | \
+                termios.ECHO   | \
+                termios.ICANON | \
+                termios.ISIG   | \
+                termios.IEXTEN)
+  fcntl.fcntl(fd, fcntl.F_SETFL, flags_save & ~os.O_NONBLOCK)
+  return fd, attrs, attrs_save, flags_save
 
+def read_key(fd,attrs,attrs_save,flags_save):
+  # TODO: Figure out how to avoid setting and resetting tc on each read
+  termios.tcsetattr(fd, termios.TCSANOW, attrs)
+  try:
+    ret = sys.stdin.read(1)
+  except KeyboardInterrupt:
+    ret = 0
+  termios.tcsetattr(fd, termios.TCSAFLUSH, attrs_save)
+  return ret
+
+def restore_stdin(fd,attrs,attrs_save,flags_save):
+  termios.tcsetattr(fd, termios.TCSAFLUSH, attrs_save)
+  fcntl.fcntl(fd, fcntl.F_SETFL, flags_save)
 
 def start(print_progress = True):
-    """
-    This is for recording a small number of events for two mice in a cage via
-    the following keypresses:
-    
-    'a': Allogrooming (grooming the unconscious mouse)
-    
-    'b': Burrowing (rummaging under bedding or other mouse)
-    
-    'c': Climbing cage
-    
-    'g': Grooming self
-    
-    'n': Nesting behavior (bringing materials or building nest)
-    
-    'r': Rearing (forepaws in air)
-    
-    's': Sitting
-    
-    'o': Other (stop indicator or other unscored behavior).
-    """
-    import time
-    from collections import defaultdict
-    start_time = time.time()
-    actions = defaultdict(list)
-    labels = defaultdict()
-    labels['a'] = 'Allogrooming'
-    labels['b'] = 'Burrowing'
-    labels['c'] = 'Climbing'
-    labels['g'] = 'Grooming'
-    labels['n'] = 'Nesting'
-    labels['o'] = 'Other'
-    labels['r'] = 'Rearing'
-    labels['s'] = 'Sitting'
+  """
+  This is for recording a small number of events for two mice in a cage via
+  the following keypresses:
+  
+  'a': Allogrooming (grooming the unconscious mouse)
+  
+  'b': Burrowing (rummaging under bedding or other mouse)
+  
+  'c': Climbing cage
+  
+  'g': Grooming self
+  
+  'n': Nesting behavior (bringing materials or building nest)
+  
+  'r': Rearing (forepaws in air)
+  
+  's': Sitting
+  
+  'o': Other (blank state).
 
-    # Initialize in the 'other' state
-    action_start = 0
-    action_type = 'o'
+  Additionally, 'q' quits and 'p' pauses/unpauses the recording process.
+  """
+  import time
+  from collections import defaultdict
+  time_delta = time.time()
+  paused = False
+  
+  actions = defaultdict(list)
+  labels = defaultdict()
+  labels['a'] = 'Allogrooming'
+  labels['b'] = 'Burrowing'
+  labels['c'] = 'Climbing'
+  labels['g'] = 'Grooming'
+  labels['n'] = 'Nesting'
+  labels['o'] = 'Other'
+  labels['r'] = 'Rearing'
+  labels['s'] = 'Sitting'
 
-    while True:
-        key = read_key()
-        curr_time = time.time() - start_time
-        if print_progress:
-            print 'Recorded %s after %f seconds' % (key, curr_time)
-        if key == 'q':
-            actions[action_type].append((action_start, curr_time))
-            totals = ()
-            used_labels = ()
-            for char, segments in actions.items():
-                total = 0
-                for beg, end in segments:
-                    total = total + (end - beg)
+  # Initialize in the 'other' state
+  action_start = 0
+  action_type = 'o'
 
-                totals = totals + (total,)
-                used_labels = used_labels + (labels[char],)
-                print 'action: %s (total of %f seconds)' % (char, total)
-                print segments
+  # Ready stdin for reading a single key
+  stdin_state = ready_stdin()
 
-            try:
-                import matplotlib.pyplot as plt
-                plt.pie(totals, labels=used_labels, \
-                  autopct='%1.1f%%', shadow=True)
-                plt.axis('equal')
-                plt.show()
-            except:
-                print 'WARNING: Could not import pyplot'
+  while True:
+    key = read_key(*stdin_state)
+    curr_time = time.time() - time_delta
 
-            break
-        else:
-            if key != 'a' and \
-               key != 'b' and \
-               key != 'c' and \
-               key != 'g' and \
-               key != 'n' and \
-               key != 'o' and \
-               key != 'r' and \
-               key != 's':
-                print "WARNING: Unrecognized key of %s; changing to 'o'" % key
-                key = 'o'
+    if key == 'q':
+      actions[action_type].append((action_start, curr_time))
+      totals = ()
+      used_labels = ()
+      for char, segments in actions.items():
+        total = 0
+        for beg, end in segments:
+          total = total + (end - beg)
+        totals = totals + (total,)
+        used_labels = used_labels + (labels[char],)
+        print ''
+        print '%s (%f seconds)' % (labels[char], total)
+        print segments
 
-            if action_type != key:
-                actions[action_type].append((action_start, curr_time))
-                action_type = key
-                action_start = curr_time
+      try:
+        import matplotlib.pyplot as plt
+        plt.pie(totals, labels=used_labels, \
+        autopct='%1.1f%%', shadow=True)
+        plt.axis('equal')
+        plt.show()
+      except:
+        print 'WARNING: Could not import pyplot'
+
+      break
+
+    elif key == ' ':
+      if paused:
+        pause_time = curr_time - pause_start
+        time_delta = time_delta + pause_time
+        paused = False
+        print "Ended %f second pause" % pause_time
+      else:
+        pause_start = curr_time
+        paused = True
+        print "Pausing"
+
+    elif not paused:
+      if key != 'a' and \
+         key != 'b' and \
+         key != 'c' and \
+         key != 'g' and \
+         key != 'n' and \
+         key != 'o' and \
+         key != 'r' and \
+         key != 's':
+        print "WARNING: Unrecognized key of %s; changing to 'o'" % key
+        key = 'o'
+
+      if print_progress:
+        print 'Marking as %s at %f seconds' % (labels[key], curr_time)
+
+      if action_type != key:
+        actions[action_type].append((action_start, curr_time))
+        action_type = key
+        action_start = curr_time
+
+  # Put stdin back to normal before exiting
+  restore_stdin(*stdin_state)
+
+  return actions
+
+if __name__ == "__main__":
+  start()
